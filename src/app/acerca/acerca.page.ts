090827b3c97a6dcf1caf8d033a3fbdfa2f13d104
AdrianNoguera6 <adrian.noguera@alumnojoyfe.iepgroup.es>
2026-04-13 18:04:03 +0200

    feat: añadida foto de perfil dinámica con inicial y color aleatorio en página Acerca de

--- src/app/acerca/acerca.page.ts
+++ src/app/acerca/acerca.page.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-// 1. AÑADIMOS IonList AQUÍ ARRIBA
 import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonMenuButton, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonButton, IonIcon, IonItem, IonLabel, IonList } from '@ionic/angular/standalone';
 import { Router } from '@angular/router';
 
@@ -10,13 +9,16 @@ import { Router } from '@angular/router';
   templateUrl: './acerca.page.html',
   styleUrls: ['./acerca.page.scss'],
   standalone: true,
-  // 2. AÑADIMOS IonList AQUÍ ABAJO TAMBIÉN
   imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonMenuButton, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonButton, IonIcon, IonItem, IonLabel, IonList]
 })
 export class AcercaPage implements OnInit {
 
   // Variable para guardar tus datos
   usuarioActual: any = null;
+  
+  // Nuevas variables para el perfil
+  inicialUsuario: string = '';
+  colorFondoPerfil: string = '#3B82F6'; // Color base por defecto
 
   constructor(private router: Router) { }
 
@@ -25,7 +27,38 @@ export class AcercaPage implements OnInit {
     const usuarioGuardado = localStorage.getItem('usuarioLogueado');
     if (usuarioGuardado) {
       this.usuarioActual = JSON.parse(usuarioGuardado); // Transformamos el texto en un objeto
+      
+      // Llamamos a nuestra nueva función para preparar la foto de perfil
+      this.prepararIconoPerfil();
     }
+  }
+
+  prepararIconoPerfil() {
+    // Comprobamos que el usuario tiene un nombre definido
+    if (this.usuarioActual && this.usuarioActual.nombre) {
+      // Obtenemos la primera letra (índice 0)
+      this.inicialUsuario = this.usuarioActual.nombre.charAt(0);
+      // Asignamos un color al azar
+      this.colorFondoPerfil = this.generarColorAleatorio();
+    }
+  }
+
+  generarColorAleatorio(): string {
+    // Lista de colores bonitos para el fondo (Estilo Tailwind)
+    const colores = [
+      '#EF4444', // Rojo
+      '#F97316', // Naranja
+      '#F59E0B', // Ámbar
+      '#10B981', // Esmeralda
+      '#06B6D4', // Cian
+      '#3B82F6', // Azul
+      '#8B5CF6', // Violeta
+      '#EC4899'  // Rosa
+    ];
+    
+    // Generamos un número aleatorio entre 0 y la cantidad de colores
+    const indiceAleatorio = Math.floor(Math.random() * colores.length);
+    return colores[indiceAleatorio];
   }
 
   cerrarSesion() {
